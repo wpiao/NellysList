e9daabea988d06b11f68b5f1ccc6d59974bfc128
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-15 11:49:04 -0700

    working connection between client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch("/api/users");
+      const users = await res.json();
+      console.log("users", users);
+      setData(users);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>Users</h1>
+      {data.map((user, i) => {
+        return (
+          <li key={i}>
+            {user.firstName} {user.lastName}
+          </li>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
 export default App;
