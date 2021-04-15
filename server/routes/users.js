2631f25d96242400891a14ad151c96c3facc7f64
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-15 11:49:42 -0700

    working connection between client and server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  const users = [
+    { id: 1, firstName: "John", lastName: "Smith" },
+    { id: 2, firstName: "Joe", lastName: "Schmoe" },
+  ];
+  res.json(users);
 });
 
 module.exports = router;
