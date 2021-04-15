015ef68dbd1299e22758709ed0154500f49e2fd7
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-15 15:58:21 -0700

    update script and app.js

--- server/app.js
+++ server/app.js
@@ -17,12 +17,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build'));
-});
+
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
