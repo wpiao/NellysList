332231c457fcb9cfaf8a5179136f5709c7cf8d75
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-16 13:39:10 -0700

    header and footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { AdDeck } from './AdDeck';
 import data from '../mock-data';
 import './App.css';
+import { Header } from './Header';
+import { Footer } from './Footer';
 
 const App = () => {
   return (
     <>
+      <Header />
       <AdDeck ads={data.ads} />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
