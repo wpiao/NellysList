d42ae121da06b651f3a42520a9d06d7fa14a073a
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-16 12:10:48 -0700

    change var to const

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var adsRouter = require('./routes/ads');
+const adsRouter = require('./routes/ads');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
