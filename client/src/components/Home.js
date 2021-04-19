6ab5da203d5f651e74fa75cb58cccada38326709
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-19 13:44:57 -0700

    Add / and /ads/create route

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { AdDeck } from './AdDeck';
-import { CreateAdForm } from './CreateAdForm';
+import { CreateAdFormWrapper } from './CreateAdFormWrapper';
 import { useGetAds } from '../hooks/useGetAds';
 
 export const Home = () => {
   const ads = useGetAds();
   return (
-    <Router>
-      <Switch>
-        <Route
-          path="/" exact
-          children={<AdDeck ads={ads} />}
-        />
-        <Route
-          path="/ads/create"
-          children={<CreateAdForm />}
-        />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route
+        path="/" exact
+        children={<AdDeck ads={ads} />}
+      />
+      <Route
+        path="/ads/create"
+        children={<CreateAdFormWrapper />}
+      />
+    </Switch>
   );
 };
