d00db919348c43f13671eab76710a6cece142a61
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-19 10:32:48 -0700

    connect submit btn to api

--- client/src/components/CreateAdFormWrapper.js
+++ client/src/components/CreateAdFormWrapper.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { CreateAdForm } from './CreateAdForm';
+import { postAds } from '../api/apiUtils';
 
 export const CreateAdFormWrapper = () => {
-  const createAd = (ad) => {
-    // TODO: Call POST /api/ads
+  const createAd = async (ad) => {
+    try {
+      const id = await postAds(ad);
+      console.log('id', id);
+      // TODO: success message, send back to home page
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // TODO: Used for PUT /api/ad (useParams from react-router-dom)
