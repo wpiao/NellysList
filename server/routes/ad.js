459ee511a8df4e875490ccd3ab65ed16944078ab
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-19 13:37:21 -0700

    convert router.put to async and convert GET request to async await

--- server/routes/ad.js
+++ server/routes/ad.js
@@ -7,7 +7,7 @@ const sheets = google.sheets('v4');
 const moment = require('moment');
 let sheetRange = 'Ads!A2:J';
 
-router.put('/', (req, res, next) => {
+router.put('/', async (req, res, next) => {
   const modifiedDate = moment.utc().format('YYYY-MM-DD HH:mm:ss');
   const {
     id,
@@ -19,60 +19,79 @@ router.put('/', (req, res, next) => {
     email,
     zipCode,
   } = req.body;
-  sheets.spreadsheets.values.get(
-    {
+
+  const fetchGetSheets = async () => {
+    const req = {
       auth: jwtClient,
       spreadsheetId: spreadsheetId,
       range: sheetRange,
-    },
-    (err, response) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Ad retrieved from ' + sheetRange);
-        const ads = response.data.values;
-        let rowCounter = 2;
-        for (let ad of ads) {
-          if (ad[0] === req.body.id) {
-            let values = [
-              [
-                req.body.id,
-                title,
-                price,
-                description,
-                photo,
-                condition,
-                email,
-                zipCode,
-                modifiedDate,
-              ],
-            ];
-            const resource = { values };
-            return sheets.spreadsheets.values.update(
-              {
-                auth: jwtClient,
-                spreadsheetId: spreadsheetId,
-                range: `Ads!A${rowCounter}:J${rowCounter}`,
-                valueInputOption: 'USER_ENTERED',
-                resource,
-              },
-              (err, response) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log('ad');
-                  const ads = response.data.values;
-                  return res.status(204).json();
-                }
-              }
-            );
-          }
-          rowCounter++;
-        }
-        return res.status(404).json('Ad Not Found. Please check ID.');
-      }
+    };
+
+    try {
+      const response = await sheets.spreadsheets.values.get(req);
+      return response;
+    } catch (err) {
+      console.log(err);
     }
-  );
+  };
+
+  const response = await fetchGetSheets();
+  return res.json(response.data);
+
+  // sheets.spreadsheets.values.get(
+  //   {
+  //     auth: jwtClient,
+  //     spreadsheetId: spreadsheetId,
+  //     range: sheetRange,
+  //   },
+  //   (err, response) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       console.log('Ad retrieved from ' + sheetRange);
+  //       const ads = response.data.values;
+  //       let rowCounter = 2;
+  //       for (let ad of ads) {
+  //         if (ad[0] === req.body.id) {
+  //           let values = [
+  //             [
+  //               req.body.id,
+  //               title,
+  //               price,
+  //               description,
+  //               photo,
+  //               condition,
+  //               email,
+  //               zipCode,
+  //               modifiedDate,
+  //             ],
+  //           ];
+  //           const resource = { values };
+  //           return sheets.spreadsheets.values.update(
+  //             {
+  //               auth: jwtClient,
+  //               spreadsheetId: spreadsheetId,
+  //               range: `Ads!A${rowCounter}:J${rowCounter}`,
+  //               valueInputOption: 'USER_ENTERED',
+  //               resource,
+  //             },
+  //             (err, response) => {
+  //               if (err) {
+  //                 console.log(err);
+  //               } else {
+  //                 console.log('ad');
+  //                 const ads = response.data.values;
+  //                 return res.status(204).json();
+  //               }
+  //             }
+  //           );
+  //         }
+  //         rowCounter++;
+  //       }
+  //       return res.status(404).json('Ad Not Found. Please check ID.');
+  //     }
+  //   }
+  // );
 });
 
 module.exports = router;
