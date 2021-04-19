4ccf0d7fed7e628d700c09ab41342aafae8b343c
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-19 09:34:42 -0700

    Add Router to Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AdDeck } from './AdDeck';
 import { useGetAds } from '../hooks/useGetAds';
 
 export const Home = () => {
   const ads = useGetAds();
-  return <AdDeck ads={ads} />;
+  return (
+    <Router>
+      <>
+        <Switch>
+          <Route
+            path='/'
+            children={
+              <AdDeck ads={ads} />
+            }
+          />
+        </Switch>
+      </>
+    </Router>
+  );
 };
