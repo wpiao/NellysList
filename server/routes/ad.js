7d391ba883bd9e4243d294ffc9f6d6ba037417d4
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-19 15:14:04 -0700

    remove old commented code

--- server/routes/ad.js
+++ server/routes/ad.js
@@ -89,61 +89,6 @@ router.put('/', async (req, res, next) => {
   } catch (err) {
     console.log(err);
   }
-
-  // sheets.spreadsheets.values.get(
-  //   {
-  //     auth: jwtClient,
-  //     spreadsheetId: spreadsheetId,
-  //     range: sheetRange,
-  //   },
-  //   (err, response) => {
-  //     if (err) {
-  //       console.log(err);
-  //     } else {
-  //       console.log('Ad retrieved from ' + sheetRange);
-  //       const ads = response.data.values;
-  //       let rowCounter = 2;
-  //       for (let ad of ads) {
-  //         if (ad[0] === req.body.id) {
-  //           let values = [
-  //             [
-  //               req.body.id,
-  //               title,
-  //               price,
-  //               description,
-  //               photo,
-  //               condition,
-  //               email,
-  //               zipCode,
-  //               modifiedDate,
-  //             ],
-  //           ];
-  //           const resource = { values };
-  //           return sheets.spreadsheets.values.update(
-  //             {
-  //               auth: jwtClient,
-  //               spreadsheetId: spreadsheetId,
-  //               range: `Ads!A${rowCounter}:J${rowCounter}`,
-  //               valueInputOption: 'USER_ENTERED',
-  //               resource,
-  //             },
-  //             (err, response) => {
-  //               if (err) {
-  //                 console.log(err);
-  //               } else {
-  //                 console.log('ad');
-  //                 const ads = response.data.values;
-  //                 return res.status(204).json();
-  //               }
-  //             }
-  //           );
-  //         }
-  //         rowCounter++;
-  //       }
-  //       return res.status(404).json('Ad Not Found. Please check ID.');
-  //     }
-  //   }
-  // );
 });
 
 module.exports = router;
