7e4ff4544cebdbc8ec4236f783c35144582c7fef
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-19 15:13:34 -0700

    convert findRowById to promise, await 3 promises in sequential dependent order, return final response

--- server/routes/ad.js
+++ server/routes/ad.js
@@ -7,16 +7,6 @@ const sheets = google.sheets('v4');
 const moment = require('moment');
 let sheetRange = 'Ads!A2:J';
 
-const findRowById = (rows, id) => {
-  rows.forEach((row, index) => {
-    if (row[0] === id) {
-      // account for first row being the column names
-      return index + 2;
-    }
-  });
-  return null;
-};
-
 router.put('/', async (req, res, next) => {
   const modifiedDate = moment.utc().format('YYYY-MM-DD HH:mm:ss');
   const {
@@ -30,15 +20,72 @@ router.put('/', async (req, res, next) => {
     zipCode,
   } = req.body;
 
-  const getSheetsReq = {
+  const sheetsReq = {
     auth: jwtClient,
     spreadsheetId: spreadsheetId,
-    range: sheetRange,
+  };
+
+  // Helper method for processing data
+  const findRowById = async (rows, id) => {
+    let processedData = null;
+
+    rows.forEach((row, index) => {
+      // Match found
+      if (row[0] === id) {
+        // Row is found at index + 2 to account for first row of column headers
+        const row = index + 2;
+
+        const values = [
+          [
+            id,
+            title,
+            price,
+            description,
+            photo,
+            condition,
+            email,
+            zipCode,
+            modifiedDate,
+          ],
+        ];
+
+        processedData = {
+          resource: { values },
+          sheetRange: `Ads!A${row}:J${row}`,
+        };
+      }
+    });
+
+    if (processedData === null) {
+      throw Error('No matching ID found');
+    }
+
+    return processedData;
   };
 
   try {
-    const response = await sheets.spreadsheets.values.get(getSheetsReq);
-    return res.json(response.data);
+    // GET all rows in sheet
+    const getRes = await sheets.spreadsheets.values.get({
+      ...sheetsReq,
+      range: sheetRange,
+    });
+
+    // Process response to set up following PUT request
+    const processedData = await findRowById(getRes.data.values, id);
+
+    // PUT row in sheet with desired row values and sheetrange
+    const putSheetsRes = await sheets.spreadsheets.values.update({
+      ...sheetsReq,
+      range: processedData.sheetRange,
+      valueInputOption: 'USER_ENTERED',
+      resource: processedData.resource,
+    });
+
+    if (putSheetsRes.status !== 200) {
+      throw Error('Something went wrong. Please try again.');
+    }
+
+    return res.status(204).json();
   } catch (err) {
     console.log(err);
   }
