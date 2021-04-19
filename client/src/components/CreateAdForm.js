2252851e38ed94fe3c3713934c3cc605a3ef518f
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-19 12:07:57 -0700

    refactor onchange

--- client/src/components/CreateAdForm.js
+++ client/src/components/CreateAdForm.js
@@ -2,36 +2,30 @@ import React, { useState } from 'react';
 import { Container, Col, Form, Button } from 'react-bootstrap';
 import { CONDITION } from './Condition';
 
+const AD_INPUTS = {
+  ID: 'id',
+  TITLE: 'title',
+  PRICE: 'price',
+  DESCRIPTION: 'description',
+  PHOTO: 'photo',
+  CONDITION: 'condition',
+  EMAIL: 'email',
+  ZIP_CODE: 'zipCode',
+};
+
 export const CreateAdForm = (props) => {
   const { id, handleSubmit } = props;
   const [ad, setAd] = useState({});
 
-  const handleTitleChange = (e) => {
-    setAd({ ...ad, title: e.target.value });
-  };
-
-  const handleDescriptionChange = (e) => {
-    setAd({ ...ad, description: e.target.value });
+  const handleOnChange = (e, adKey) => {
+    setAd({ ...ad, [adKey]: e.target.value });
   };
 
   const handlePriceChange = (e) => {
-    setAd({ ...ad, price: e.target.value });
-  };
-
-  const handleConditionChange = (e) => {
-    setAd({ ...ad, condition: e.target.value });
-  };
-
-  const handlePhotoURLChange = (e) => {
-    setAd({ ...ad, photoUrl: e.target.value });
-  };
-
-  const handleEmailChange = (e) => {
-    setAd({ ...ad, email: e.target.value });
-  };
-
-  const handleZipCodeChange = (e) => {
-    setAd({ ...ad, zipCode: e.target.value });
+    setAd({
+      ...ad,
+      price: parseFloat(e.target.value),
+    });
   };
 
   const submit = (e) => {
@@ -44,23 +38,23 @@ export const CreateAdForm = (props) => {
       <Form onSubmit={submit}>
         <Form.Row>
           <Col xs={6}>
-            <Form.Group controlId="title">
+            <Form.Group controlId={AD_INPUTS.TITLE}>
               <Form.Label>Title</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter Title"
-                onChange={handleTitleChange}
+                onChange={(e) => handleOnChange(e, AD_INPUTS.TITLE)}
               />
             </Form.Group>
-            <Form.Group controlId="description">
+            <Form.Group controlId={AD_INPUTS.DESCRIPTION}>
               <Form.Label>Description</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter Description"
-                onChange={handleDescriptionChange}
+                onChange={(e) => handleOnChange(e, AD_INPUTS.DESCRIPTION)}
               />
             </Form.Group>
-            <Form.Group controlId="price">
+            <Form.Group controlId={AD_INPUTS.PRICE}>
               <Form.Label>Price</Form.Label>
               <Form.Control
                 type="text"
@@ -68,38 +62,42 @@ export const CreateAdForm = (props) => {
                 onChange={handlePriceChange}
               />
             </Form.Group>
-            <Form.Group controlId="condition">
+            <Form.Group controlId={AD_INPUTS.CONDITION}>
               <Form.Label>Condition</Form.Label>
-              <Form.Control as="select" custom onChange={handleConditionChange}>
+              <Form.Control
+                as="select"
+                custom
+                onChange={(e) => handleOnChange(e, AD_INPUTS.CONDITION)}
+              >
                 <option>Select Condition</option>
                 <option>{CONDITION.NEW}</option>
                 <option>{CONDITION.OLD}</option>
               </Form.Control>
             </Form.Group>
-            <Form.Group controlId="photo-url">
+            <Form.Group controlId={AD_INPUTS.PHOTO}>
               <Form.Label>Photo URL</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter Photo URL"
-                onChange={handlePhotoURLChange}
+                onChange={(e) => handleOnChange(e, AD_INPUTS.PHOTO)}
               />
             </Form.Group>
           </Col>
           <Col xs={6}>
-            <Form.Group controlId="email">
+            <Form.Group controlId={AD_INPUTS.EMAIL}>
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter Email"
-                onChange={handleEmailChange}
+                onChange={(e) => handleOnChange(e, AD_INPUTS.EMAIL)}
               />
             </Form.Group>
-            <Form.Group controlId="zip-code">
+            <Form.Group controlId={AD_INPUTS.ZIP_CODE}>
               <Form.Label>Zip Code</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter Zip Code"
-                onChange={handleZipCodeChange}
+                onChange={(e) => handleOnChange(e, AD_INPUTS.ZIP_CODE)}
               />
             </Form.Group>
             <Button
