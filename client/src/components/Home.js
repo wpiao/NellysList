647f631afcb6895be6db1f484a43b1a3fa70ba2d
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-19 08:31:31 -0700

    Remove mock data and connect the data from server

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { AdDeck } from './AdDeck';
-import data from '../mock-data';
 import { useGetAds } from '../hooks/useGetAds';
 
 export const Home = () => {
   const ads = useGetAds();
-  return <AdDeck ads={data.ads} />;
+  return <AdDeck ads={ads} />;
 };
