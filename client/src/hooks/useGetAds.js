684ce1a545ed161f81038536b9a60a9712f31401
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-19 09:57:55 -0700

    fix warnings

--- client/src/hooks/useGetAds.js
+++ client/src/hooks/useGetAds.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getAds } from '../api/apiUtils';
 
 export const useGetAds = () => {
   const [ads, setAds] = useState([]);
 
-  useEffect(async () => {
-    const ads = await getAds();
-    setAds(ads);
-  }, []);
+  useEffect(() => {
+    const fetchGetAds = async () => {
+      const ads = await getAds();
+      setAds(ads);
+    };
+    fetchGetAds();
+  }, [input]);
 
   return ads;
 };
