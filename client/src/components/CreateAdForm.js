1937a32fc608dc2db41c8794c651c84f414004ac
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-20 15:26:31 -0700

    validation complete

--- client/src/components/CreateAdForm.js
+++ client/src/components/CreateAdForm.js
@@ -13,53 +13,69 @@ const AD_INPUTS = {
   ZIP_CODE: 'zipCode',
 };
 
+const REGEX = {
+  PHOTO: '^(https?:\\/\\/.*\\.(?:jpg|jpeg|gif|png))$',
+  ZIP_CODE: '(^\\d{5}$)|(^\\d{9}$)|(^\\d{5}-\\d{4}$)',
+};
+
 export const CreateAdForm = ({ id, handleSubmit }) => {
   const [ad, setAd] = useState({});
+  const [validated, setValidated] = useState(false);
 
   const handleOnChange = (e, adKey) => {
-    setAd({ ...ad, [adKey]: e.target.value });
-  };
-
-  const handlePriceChange = (e) => {
-    setAd({
-      ...ad,
-      price: parseFloat(e.target.value),
-    });
+    const value =
+      e.target.type === 'number' ? parseFloat(e.target.value) : e.target.value;
+    setAd({ ...ad, [adKey]: value });
   };
 
   const submit = (e) => {
-    e.preventDefault();
-    handleSubmit(ad, e);
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    } else {
+      handleSubmit(ad, e);
+    }
+    setValidated(true);
   };
 
   return (
     <Container>
-      <Form onSubmit={submit}>
+      <Form onSubmit={submit} noValidate validated={validated}>
         <Form.Row>
           <Col xs={6}>
             <Form.Group controlId={AD_INPUTS.TITLE} className="required">
               <Form.Label>Title</Form.Label>
               <Form.Control
+                required
                 type="text"
                 placeholder="Enter Title"
                 onChange={(e) => handleOnChange(e, AD_INPUTS.TITLE)}
               />
+              <Form.Control.Feedback type="invalid">
+                Please enter a title.
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId={AD_INPUTS.DESCRIPTION} className="required">
               <Form.Label>Description</Form.Label>
               <Form.Control
+                required
                 as="textarea"
                 rows={3}
                 placeholder="Enter Description"
                 onChange={(e) => handleOnChange(e, AD_INPUTS.DESCRIPTION)}
               />
+              <Form.Control.Feedback type="invalid">
+                Please enter a description.
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId={AD_INPUTS.PRICE}>
               <Form.Label>Price</Form.Label>
               <Form.Control
-                type="text"
+                type="number"
                 placeholder="Enter Price"
-                onChange={handlePriceChange}
+                onChange={(e) => handleOnChange(e, AD_INPUTS.PRICE)}
+                onKeyPress={(e) => e.charCode === 46 && e.preventDefault()}
               />
             </Form.Group>
             <Form.Group controlId={AD_INPUTS.CONDITION}>
@@ -69,16 +85,16 @@ export const CreateAdForm = ({ id, handleSubmit }) => {
                 custom
                 onChange={(e) => handleOnChange(e, AD_INPUTS.CONDITION)}
               >
-                <option>Select Condition</option>
-                <option>{CONDITION.NEW}</option>
                 <option>{CONDITION.OLD}</option>
+                <option>{CONDITION.NEW}</option>
               </Form.Control>
             </Form.Group>
             <Form.Group controlId={AD_INPUTS.PHOTO}>
               <Form.Label>Photo URL</Form.Label>
               <Form.Control
+                pattern={REGEX.PHOTO}
                 type="text"
-                placeholder="Enter Photo URL"
+                placeholder="http://"
                 onChange={(e) => handleOnChange(e, AD_INPUTS.PHOTO)}
               />
             </Form.Group>
@@ -87,18 +103,27 @@ export const CreateAdForm = ({ id, handleSubmit }) => {
             <Form.Group controlId={AD_INPUTS.EMAIL} className="required">
               <Form.Label>Email</Form.Label>
               <Form.Control
-                type="text"
+                required
+                type="email"
                 placeholder="Enter Email"
                 onChange={(e) => handleOnChange(e, AD_INPUTS.EMAIL)}
               />
+              <Form.Control.Feedback type="invalid">
+                Please enter a valid email.
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId={AD_INPUTS.ZIP_CODE} className="required">
               <Form.Label>Zip Code</Form.Label>
               <Form.Control
+                required
+                pattern={REGEX.ZIP_CODE}
                 type="text"
                 placeholder="Enter Zip Code"
                 onChange={(e) => handleOnChange(e, AD_INPUTS.ZIP_CODE)}
               />
+              <Form.Control.Feedback type="invalid">
+                Please enter a zip code.
+              </Form.Control.Feedback>
             </Form.Group>
             <Button
               variant="primary"
