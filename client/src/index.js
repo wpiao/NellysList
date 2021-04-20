e9c9d9620ef6f79710a9b98cb414cca05ca84397
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-20 09:21:32 -0700

    wrap App component with AlertProvider

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,25 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+// optional configuration
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.TOP_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE,
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+      </AlertProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
