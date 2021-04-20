1daddbf27b4e20c566d6e56b0c8cf7c02468ce4f
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-20 09:53:17 -0700

    Add /ad/:id route

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { AdDeck } from './AdDeck';
 import { CreateAdFormWrapper } from './CreateAdFormWrapper';
 import { useGetAds } from '../hooks/useGetAds';
+import { AdDetails } from './AdDetails';
 
 export const Home = () => {
   const ads = useGetAds();
+  const [ad, setAd] = useState({});
+
   return (
     <Switch>
       <Route
         path="/" exact
-        children={<AdDeck ads={ads} />}
+        children={<AdDeck ads={ads} setAd={setAd} />}
       />
       <Route
         path="/ads/create"
         children={<CreateAdFormWrapper />}
+      />
+      <Route
+        path={`/ad/${ad.id}`}
+        children={<AdDetails ad={ad} />}
       />
     </Switch>
   );
