f17c54774321236821f57d5090fd8b1e33fb8d92
Chaynor Hsiao <chaynsaw@gmail.com>
2021-04-20 14:40:55 -0700

    Refactor to async/await

--- server/routes/ads.js
+++ server/routes/ads.js
@@ -9,48 +9,47 @@ const moment = require('moment');
 let sheetRange = 'Ads!A2:J';
 
 // get all ads
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   let id = req.query.id;
-  sheets.spreadsheets.values.get(
-    {
-      auth: jwtClient,
-      spreadsheetId: spreadsheetId,
-      range: sheetRange,
-    },
-    (err, response) => {
-      if (err) {
-        console.log('The API returned an error: ' + err);
-      } else {
-        console.log('Ads retrieved from ' + sheetRange);
-        const ads = response.data.values;
-        let result;
-        // if ID query param exists, filter for that id only.
-        if (id) {
-          result = ads.filter((ad) => {
-            if (ad[0] === id) {
-              return ad;
-            }
-          });
-        } else {
-          result = ads.map((ad) => {
-            return {
-              id: ad[0],
-              title: ad[1],
-              price: ad[2],
-              description: ad[3],
-              photo: ad[4],
-              condition: ad[5],
-              email: ad[6],
-              zipCode: ad[7],
-              modifiedDate: ad[8],
-              createdDate: ad[9],
-            };
-          });
-        }
-        res.status(200).json(result);
+  const processData = async (data) => {
+    return data.map((ad) => {
+      return {
+        id: ad[0],
+        title: ad[1],
+        price: ad[2],
+        description: ad[3],
+        photo: ad[4],
+        condition: ad[5],
+        email: ad[6],
+        zipCode: ad[7],
+        modifiedDate: ad[8],
+        createdDate: ad[9],
+      };
+    });
+  };
+
+  const getRes = await sheets.spreadsheets.values.get({
+    auth: jwtClient,
+    spreadsheetId: spreadsheetId,
+    range: sheetRange,
+  });
+
+  let result = await processData(getRes.data.values);
+
+  if (!id) {
+    res.status(200).json(result);
+  } else {
+    result = result.filter((item) => {
+      if (item.id === id) {
+        return item;
       }
+    });
+    if (result.length === 0) {
+      res.status(404).json('Ad not found. Please check your id.');
+    } else {
+      res.status(200).json(result);
     }
-  );
+  }
 });
 
 // post new ad
