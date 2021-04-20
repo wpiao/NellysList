9d3b6585bb24725e5c9256acadb2654da7add04b
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-20 09:53:52 -0700

    Passing a new props

--- client/src/components/Ad.js
+++ client/src/components/Ad.js
@@ -1,10 +1,16 @@
-import React from 'react';
 import { Card } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
-export const Ad = ({ ad }) => {
+export const Ad = ({ ad, setAd }) => {
+  let history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/ad/${ad.id}`);
+    setAd(ad);
+  }
   return (
     <>
-      <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '18rem' }} onClick={handleClick} >
         <Card.Img variant="top" src={ad.photo} style={{ height: 300 }} />
         <Card.Body>
           <Card.Title style={{ textAlign: 'center' }}>{ad.title}</Card.Title>
