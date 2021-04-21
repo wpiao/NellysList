b8f63160fbd27976ed939e19369148f8ec3c2925
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-21 08:48:07 -0700

    add upload route

---
+++ server/routes/upload.js
@@ -0,0 +1,20 @@
+const { cloudinary } = require('../cloudinary/cloudinary');
+const express = require('express');
+const router = express.Router();
+
+router.post('/api/upload', async (req, res) => {
+  console.log('req', req);
+  try {
+    const fileStr = req.body.data;
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: 'nellys_list',
+    });
+    console.log(uploadResponse);
+    res.status(200).json({ message: 'success' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: 'Something went wrong' });
+  }
+});
+
+module.exports = router;
