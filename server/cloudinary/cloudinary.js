60cfb2b9524db8c08eedbf479d74daa36cbd4b78
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-21 08:50:06 -0700

    setup cloudinary config and env variables

---
+++ server/cloudinary/cloudinary.js
@@ -0,0 +1,12 @@
+var path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../config/.env') });
+
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+module.exports = { cloudinary };
