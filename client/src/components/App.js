209327aabd92cfb560858b5e1c875e938eaaa37c
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-27 08:49:32 -0700

    Create a graphQL client and Wrap Home component with ApolloProvider

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import { Home } from './Home';
 import { Header } from './Header';
 import { Footer } from './Footer';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache()
+});
 
 const App = () => {
   return (
     <>
       <Header />
-      <Home />
+      <ApolloProvider client={client}>
+        <Home />
+      </ApolloProvider>
       <Footer />
     </>
   );
