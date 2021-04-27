99e15da0fdf1b0662764780bf40c0e5327a90360
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-27 08:55:59 -0700

    Add test query to the graphQL server

--- server/app.js
+++ server/app.js
@@ -27,6 +27,33 @@ app.use(apiPrefix + '/ads', adsRouter);
 app.use(apiPrefix + '/ad', adRouter);
 app.use(apiPrefix + '/upload', uploadRouter);
 
+// sandbox
+const { graphqlHTTP } = require('express-graphql');
+const mockData = require('../client/src/mock-data');
+console.log(mockData);
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} = require('graphql');
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'HelloWorld',
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => 'Hello World'
+      }
+    })
+  })
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
+
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
