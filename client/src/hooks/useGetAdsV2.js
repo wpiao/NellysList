69702a717989d4e9ffce1832ff1114be25eb5c69
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-29 14:09:57 -0700

    Refactor getAds

--- client/src/hooks/useGetAdsV2.js
+++ client/src/hooks/useGetAdsV2.js
@@ -1,28 +1,29 @@
 import { useEffect, useContext } from 'react';
 import { AdsContext, ACTIONS } from '../contexts/AdsContext';
-import { useQuery } from '@apollo/client';
 import { GET_ADS } from '../GraphQL/queries';
+import { client } from '../components/V2App';
 
 
 export const useGetAdsV2 = () => {
   const [adsState, dispatch] = useContext(AdsContext);
-  const { ads, searchedAds, isLoadingAds } = adsState;
-  const { loading, error, data } = useQuery(GET_ADS);
+  const { ads, searchedAds, isLoadingAds, error } = adsState;
 
   useEffect(() => {
-    if (loading) {
+    const getAds = async () => {
       dispatch({ type: ACTIONS.LOAD_ADS });
+      try {
+        const res = await client.query({ query: GET_ADS });
+        dispatch({
+          type: ACTIONS.GET_ADS,
+          payload: { ads: res?.data?.ads || ads }
+        });
+      } catch (err) {
+        dispatch({ type: ACTIONS.ERROR_ADS, payload: { error: err } });
+      }
     }
-    if (error) {
-      dispatch({ type: ACTIONS.ERROR_ADS, payload: { error: error } });
-    }
-    if (data) {
-      dispatch({
-        type: ACTIONS.GET_ADS,
-        payload: { ads: data.ads },
-      });
-    }
-  }, [data]); // eslint-disable-line react-hooks/exhaustive-deps
+
+    getAds();
+  }, []);  // eslint-disable-line react-hooks/exhaustive-deps
 
   return { ads, searchedAds, isLoadingAds, error, dispatch };
 };
