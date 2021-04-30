c85c2df7bfd81b9b91962f7cfc85dc0e45407050
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-29 23:35:20 -0700

    remove v1 REST endpoints

--- client/src/api/apiUtils.js
+++ client/src/api/apiUtils.js
@@ -1,33 +1,5 @@
 import ajax from 'superagent';
 
-export const getAds = async () => {
-  try {
-    const res = await ajax.get('/api/ads');
-    return res.body.data.ads;
-  } catch (err) {
-    // will make a error component and render it when error happens
-    console.log(err);
-  }
-};
-
-export const postAds = async (ad) => {
-  try {
-    const res = await ajax.post('/api/ads').send(ad).set('accept', 'json');
-    return res.body;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const putAds = async (ad) => {
-  try {
-    const res = await ajax.put('/api/ad').send(ad).set('accept', 'json');
-    return res.body;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 export const postUpload = async (base64encodedImage) => {
   let imageUrl = null;
   try {
@@ -43,14 +15,6 @@ export const postUpload = async (base64encodedImage) => {
   return imageUrl;
 };
 
-export const deleteAd = async (id) => {
-  try {
-    await ajax.delete('/api/ads').query({ id: id });
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 export const getLatLngByZipCode = async (zipCode) => {
   const baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
   const key = process.env.REACT_APP_GOOGLE_KEY;
@@ -60,14 +24,5 @@ export const getLatLngByZipCode = async (zipCode) => {
     return res.body;
   } catch (err) {
     console.log(err);
-  }
-};
-
-export const getAd = async (id) => {
-  try {
-    const res = await ajax.get(`/api/ads?id=${id}`);
-    return res?.body?.data?.ads[0];
-  } catch (error) {
-    console.error(error);
   }
 };
